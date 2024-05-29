371fe0ac2266515237d62863162e9f46462d858b
Twave1717 <wjdrldyd0213@gmail.com>
2024-05-29 20:57:23 +0900

    test code update

--- test_finetuned_model.py
+++ test_finetuned_model.py
@@ -2,15 +2,14 @@ import torch
 from PIL import Image
 from transformers import Pix2StructProcessor, Pix2StructForConditionalGeneration
 
-processor = Pix2StructProcessor.from_pretrained('google/deplot')
-
-def load_trained_model(checkpoint_path, processor):
+def load_trained_model(checkpoint_path):
     model = Pix2StructForConditionalGeneration.from_pretrained('google/deplot')
     model.load_state_dict(torch.load(checkpoint_path))
     model.eval()
     return model
 
 def predict(image_path, model, processor):
+    processor = Pix2StructProcessor.from_pretrained('google/deplot')
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
     image = Image.open(image_path)
@@ -25,11 +24,11 @@ def print_prediction(prediction):
 
 # Load the trained model from a specific checkpoint
 checkpoint_path = "weights/deplot_format_label_epoch_39.bin"  # Specify the checkpoint you want to load
-trained_model = load_trained_model(checkpoint_path, processor)
+trained_model = load_trained_model(checkpoint_path)
 
 # Perform inference on a new image
 test_image_path = "./png_and_annotation_100/0.png"  # Specify the path to the test image
-prediction = predict(test_image_path, trained_model, processor)
+prediction = predict(test_image_path, trained_model)
 print("Prediction for the test image:")
 print('\n---------------\n')
 print_prediction(prediction)
