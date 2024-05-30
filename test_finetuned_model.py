f00aa8fe7c602ef2608ccac02c8c626ca4ffd70e
Twave1717 <wjdrldyd0213@gmail.com>
2024-05-30 15:25:57 +0900

    test code update (테스트코드는 해당 코드로 통일!)

--- test_finetuned_model.py
+++ test_finetuned_model.py
@@ -1,15 +1,15 @@
-import json
-import os
 import torch
 from PIL import Image
-import pandas as pd
-from torch.utils.data import Dataset, DataLoader
 from transformers import Pix2StructProcessor, Pix2StructForConditionalGeneration, TrainingArguments, Trainer
-from datasets import Dataset as HFDataset
 
-processor = Pix2StructProcessor.from_pretrained('google/deplot')
+### path ###
+checkpoint_path = "weights/reform2_epoch_34.bin"  # Specify the checkpoint you want to load
+test_image_path = "./png_and_annotation_100/0.png"  # Specify the path to the test image
+###
 
-def load_trained_model(checkpoint_path, processor):
+
+
+def load_trained_model(checkpoint_path):
     model = Pix2StructForConditionalGeneration.from_pretrained('google/deplot')
     model.load_state_dict(torch.load(checkpoint_path))
     model.eval()
@@ -17,20 +17,18 @@ def load_trained_model(checkpoint_path, processor):
 
 def predict(image_path, model):
     processor = Pix2StructProcessor.from_pretrained('google/deplot')
+    image = Image.open(image_path)
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
-    image = load_image(image_path)
-    inputs = processor(images=image, return_tensors="pt").to(device)
-    predictions = model.generate(**inputs)
-    decoded_predictions = processor.batch_decode(predictions, skip_special_tokens=True)
-    return decoded_predictions
+    inputs = processor(images=image, text="Generate underlying data table of the figure below:", return_tensors="pt").to(device)
+    predictions = model.generate(**inputs, max_new_tokens=512)
+    return processor.decode(predictions[0], skip_special_tokens=True)
 
-# Load the trained model from a specific checkpoint
-checkpoint_path = "weights/reform2_epoch_34.bin"  # Specify the checkpoint you want to load
+def print_prediction(prediction):
+    for line in prediction.split('0x0A'):
+        print(line)
+
 trained_model = load_trained_model(checkpoint_path)
-
-# Perform inference on a new image
-test_image_path = "./png_and_annotation_100/0.png"  # Specify the path to the test image
 prediction = predict(test_image_path, trained_model)
 print("Prediction for the test image:")
 print('\n---------------\n')
